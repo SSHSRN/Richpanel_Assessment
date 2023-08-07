d1174161f68a48fd5f0a245a0984603597df4850
Srihari <sshsrn@gmail.com>
2023-08-07 08:24:18 +0530

    fix: homepage ui update
    Signed-off-by: Srihari <sshsrn@gmail.com>

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <div className='homeContainer'>
-        <h1>MyApp</h1>
-        <h3>This website is built for the Richpanel Assessment</h3>
-        <h3>It is built using ReactJS for the frontend and Flask for the backend</h3>
-        <h3>It uses Stripe for the payment gateway and MongoDB for the database</h3>
-        <h3>It is hosted on Google Cloud Platform (Cloud Run) and CI/CD is enabled using Github Actions</h3>
-        <h3>Click <Link to="/signup">here</Link> to signup and <Link to="/login">here</Link> to login</h3>
+      <h1 className='text-center'>MyApp</h1>
+      <h3 className='mt-5'>This website is built for the Richpanel Assessment.</h3>
+      <h3>The website is built using ReactJS for the frontend, Flask for the backend.</h3>
+      <h3>The website uses Stripe for the payment gateway, MongoDB for the database.</h3>
+      <h3>The website is hosted on Google Cloud Platform (Cloud Run) and CI/CD is enabled using Github Actions.</h3>
+      <h3 className='mt-5'>Click <Link to="/signup">here</Link> to signup and <Link to="/login">here</Link> to login</h3>
     </div>
   )
 }
